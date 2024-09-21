67472738a7201db77076b862d611376f0dc4dddf
Nero-Xrpl5 <sulungdinero@gmail.com>
2024-09-21 20:58:01 +0700

    Add files via upload

---
+++ perulanganPeerkalian.java
@@ -0,0 +1,24 @@
+package Perulangan;
+
+import java.util.Scanner;
+
+public class perulanganPeerkalian {
+  public static void main(String[] args) {
+    int angka, temp=1;
+    
+    Scanner s = new Scanner (System.in);
+
+    System.out.println("Masukkan Angka!");
+    angka = s.nextInt();
+    
+    for (int i = angka; i >= 1; i--) {
+      System.out.print(i+" x ");
+      temp *=i;
+      
+    }
+    System.out.println();
+    System.out.println("Total faktorial: "+temp);
+
+    }
+  
+}
